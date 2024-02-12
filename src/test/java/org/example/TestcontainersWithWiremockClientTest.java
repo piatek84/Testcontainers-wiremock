b8e1bbdfdbe2f8ade0e395949548e8d2033c14f9
Juan Coronel <juan.coronel@lottoland.com>
2024-02-12 13:29:52 +0100

    initial commit

--- src/test/java/org/example/TestcontainersWithWiremockClientTest.java
+++ src/test/java/org/example/TestcontainersWithWiremockClientTest.java
@@ -1,7 +1,6 @@
 package org.example;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
-import org.hamcrest.CoreMatchers;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -10,6 +9,9 @@ import org.wiremock.integrations.testcontainers.WireMockContainer;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.awaitility.Awaitility.await;
+
 
 @Testcontainers
 class TestcontainersWithWiremockClientTest {
@@ -38,16 +40,15 @@ class TestcontainersWithWiremockClientTest {
                         )
         );
 
-        given()
-                .header("Content-Type", "application/json")
-                .queryParam("name", name)
-                .body(body)
-
+        await().untilAsserted(() -> assertThat(
+                given()
+                        .header("Content-Type", "application/json")
+                        .queryParam("name", name)
+                        .body(body)
                 .when()
-                .post(wireMockServer.getUrl("/dynamic/"+ path))
-
+                        .post(wireMockServer.getUrl("/dynamic/"+ path))
                 .then()
-                .statusCode(200)
-                .body("message", CoreMatchers.equalTo(path + " " + name));
+                        .statusCode(200)
+                        .extract().path("message").toString()).isEqualTo(path + "a " + name));
     }
 }
